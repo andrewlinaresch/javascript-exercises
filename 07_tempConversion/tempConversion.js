312972bbcaff63191103045937423a482ab992a6
Andrew Linares <andrewlinaresch@gmail.com>
2023-09-15 11:33:31 -0400

    solved for 1st test of both modules

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahr) {
+  return (tempFahr - 32) * (5/9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCels) {
+  return tempCels * (9/5) + 32;
 };
 
 // Do not edit below this line
