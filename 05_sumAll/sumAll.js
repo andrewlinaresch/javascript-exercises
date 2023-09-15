46ae30dd7f45ce48444185c90b89c9d217851548
Andrew Linares <andrewlinaresch@gmail.com>
2023-09-14 23:33:26 -0400

    Checked and passed the rest of the test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
 const sumAll = function(num1, num2) {
-    if (typeof num1 != 'number' || typeof num2 != 'number') return 'Error'
+    //checks validity of arguments (negative or non-numerical)
+    if (typeof num1 != 'number' || typeof num2 != 'number') return 'ERROR'
+    if (num1 < 0 || num2 < 0) return 'ERROR'
+    
     let sum = 0;
+    
     if (num2 > num1){
         let difference = num2-num1;
         for (let i = 0; i < num2; i++) sum += (num1 + i);
