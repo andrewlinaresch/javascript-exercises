c2176f6b6e738bb2b7d5daccb33652f6228647cd
Andrew Linares <andrewlinaresch@gmail.com>
2023-09-14 22:30:06 -0400

    solved 04 for first test case

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, rElement) {
+    let newArray = []
+    for (element of array){
+        if (element != rElement){
+            newArray.push(element);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
