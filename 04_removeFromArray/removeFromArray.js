848ecf1ac359e7d77fc14911786765387710d1d2
Andrew Linares <andrewlinaresch@gmail.com>
2023-09-14 22:49:18 -0400

    refactored to a function that takes away 1 element

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,18 @@
-const removeFromArray = function(array, rElement) {
+const removeFromArray = function(array) {
     let newArray = []
+    newArray = removeElement(array,arguments[1]);
+    return newArray;
+};
+
+function removeElement(array, rElement){
+    let newArray = [];
     for (element of array){
         if (element != rElement){
             newArray.push(element);
         }
     }
     return newArray;
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
